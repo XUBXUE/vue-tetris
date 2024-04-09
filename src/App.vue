873d1e96de64e2fe5e20089d0f550094c05c9db1
XUBXUE <2370237499@qq.com>
2024-04-10 00:57:09 +0800

    feat: block move

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,77 @@
 import { ref } from "vue";
 import Block from './components/block.vue';
 
+interface IShape {
+  block: number[];
+  positionY: number;
+}
+
+let timer: number;
+
 const width = 10;
 
-const height = 20
+const height = 10;
 
 const board = ref<number[]>(
   Array.from({ length: height }, () => 0),
 );
 
-// const currentShape = ref();
+/**
+ * 0000110000
+ * 0000110000
+ */
+const square = [48, 48];
+
+/** 0001111000 */
+const line = [120];
+
+const blocks = [square, line];
+
+let currentShape: IShape;
+
+const generateShape = (block: number[]): IShape  => {
+  return {
+    block,
+    positionY: -1,
+  };
+};
 
 const convertToBinary = (num: number, length: number) => {
   let binary = num.toString(2);
 
   if (binary.length > length) return binary;
 
-  return binary.padStart(length, binary);
+  return binary.padStart(length, '0');
 };
 
-// const generateShape = () => {
-//   const square = [];
-// };
+const down = () => {
+  const { positionY, block } = currentShape;
+  const blockHeight = block.length;
+  let nextPositionY = positionY + 1;
+  let nextBoardRow = board.value[nextPositionY];
+  currentShape.positionY = nextPositionY;
+
+  // 如果当前形状位置下方有其他方块时，停止下降
+  if ((block[blockHeight - 1] & nextBoardRow) > 0) {
+    clearInterval(timer);
+    return;
+  };
+
+  for (let i = blockHeight - 1; i >= 0; i--) {
+    board.value[nextPositionY] = nextBoardRow | block[blockHeight - 1];
+
+    if (nextPositionY < blockHeight - 1) break;
+    nextBoardRow = board.value[--nextPositionY];
+  }
+};
+
+const start = () => {
+  currentShape = generateShape(blocks[Math.floor(Math.random() * blocks.length)]);
+  console.log(currentShape);
+  timer = setInterval(down, 1000);
+};
+
+start();
 
 // 1 0   0 0   0 2   0 0
 // 0 0   0 1   1 2   1 0
