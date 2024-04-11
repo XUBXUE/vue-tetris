917d3da06b233fa7e00075d9922f64a3ff179014
XUBXUE <2370237499@qq.com>
2024-04-12 01:22:27 +0800

    feat: down

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,11 @@ interface IShape {
   positionY: number;
 }
 
-let timer: number;
+let timer: number | null;
 
 const width = 10;
 
 const height = 10;
-
-const board = ref<number[]>(
-  Array.from({ length: height }, () => 0),
-);
 
 /**
  * 0000110000
@@ -28,7 +24,14 @@ const line = [120];
 
 const blocks = [square, line];
 
-let currentBlock: IShape;
+// 当前方块
+let currentBlock: IShape | null;
+
+const board = ref<number[]>(
+  Array.from({ length: height }, () => 0),
+);
+
+const state = ref<'play' | 'pause'>('pause');
 
 const generateShape = (block: number[]): IShape  => {
   return {
@@ -46,43 +49,72 @@ const convertToBinary = (num: number, length: number) => {
 };
 
 const down = () => {
-  console.log('1')
+  console.log('debug')
   // 当前方块信息
-  const { positionY, block } = currentBlock;
+  const { positionY, block } = currentBlock!;
   // 方块的高度
   const blockHeight = block.length;
   // 将要下降的纵向坐标索引
   let nextPositionY = positionY + 1;
   // 将要下降的行的信息
   let nextBoardRow = board.value[nextPositionY];
-  currentBlock.positionY = nextPositionY;
 
   // 如果当前形状位置下方有其他方块或下降到最底下时停止下降
   if ((block[blockHeight - 1] & nextBoardRow) > 0 || nextPositionY >= height) {
-    clearInterval(timer);
+    // 清空所有
+    timer && clearInterval(timer);
+    timer = null;
+    currentBlock = null;
+    // 重新开始
+    start();
     return;
   };
 
-  for (let i = blockHeight - 1; i >= 0; i--) {
-    board.value[nextPositionY] = nextBoardRow | block[blockHeight - 1];
+  // 把当前块所占位置清除掉
+  for (let i = blockHeight - 1, y = positionY; i >= 0; i--) {
+    // 如果索引小于零证明到头了，就不用继续清了
+    if (y < 0) break;
 
-    if (nextPositionY < blockHeight - 1) break;
-    nextBoardRow = board.value[--nextPositionY];
+    // 先用当前格子行取反，再与方块行取或，再取反，得到方块去除后的格子
+    let currentClearRow = ~board.value[y];
+    board.value[y--] = ~(currentClearRow | block[i]);
+  }
+
+  // 更新坐标索引
+  currentBlock!.positionY = nextPositionY;
+
+  for (let i = blockHeight - 1, y = nextPositionY; i >= 0; i--) {
+    // 更新格子行
+    board.value[y] = nextBoardRow | block[blockHeight - 1];
+
+    // 如果最底下位置没有方块高度大，就不渲染多余的，跳出循环
+    if (y < blockHeight - 1) break;
+    nextBoardRow = board.value[--y];
   }
 };
 
 const start = () => {
-  currentBlock = generateShape(blocks[Math.floor(Math.random() * blocks.length)]);
-  console.log(currentBlock);
-  timer = setInterval(down, 1000);
+  if (!currentBlock) {
+    currentBlock = generateShape(blocks[Math.floor(Math.random() * blocks.length)]);
+  }
+
+  timer = setInterval(down, 500);
 };
 
-start();
+const pause = () => {
+  timer && clearInterval(timer);
+  timer = null;
+}
 
-// 1 0   0 0   0 2   0 0
-// 0 0   0 1   1 2   1 0
-// 0 1   1 1   1 1   2 0
-// 0 2   2 1   1 0   2 1
+const toggleState = () => {
+  state.value = state.value === 'play' ? 'pause' : 'play';
+
+  if (state.value === 'play') {
+    start();
+  } else {
+    pause();
+  }
+}
 </script>
 
 <template>
@@ -98,5 +130,8 @@ start();
         :placeholder="placeholder"
       />
     </div>
+  </div>
+  <div class="flex justify-center mt-2">
+    <button class="border p-1 border-gray-500/50 rounded text-sm" @click="toggleState">{{ state === 'play' ? '暂停' : '开始' }}</button>
   </div>
 </template>
